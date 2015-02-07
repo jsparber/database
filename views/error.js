f1eddfd4a3403edfdff5b5dd01a4f8772bd1a9e9
Julian Sparber <julian@sparber.net>
2015-02-07 14:32:37 +0100

    add error.js

--- views/error.js
+++ views/error.js
@@ -1,6 +1,19 @@
-extends layout
-
-block content
-  h1= message
-  h2= error.status
-  pre #{error.stack}
+var React = require('react');
+var Content = require('./Content');
+module.exports = React.createClass({
+	render: function() {
+		console.log(this.props);
+				return (
+					<html lang="en">
+					<head>
+					<meta name="viewport" content="width=device-width, initial-scale=1"/>
+					<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
+					</head>
+					<body>
+					<div id="content"> {this.props.error} </div>
+					<div id="message"> {this.props.message} </div>
+					</body>
+					</html>
+					);
+				}
+				});
